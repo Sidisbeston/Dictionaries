e944d87be2b3a5c611b66d80a67c8e246255df01
Sidisbeston <75677056+Sidisbeston@users.noreply.github.com>
2024-07-22 18:51:34 +0200

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,12 @@
+movies = ("life of someone", "life of another person", "another life of someone", "life of another person", "different persons life")
+print (movies)
+pos = 90, 50
+print (pos)
+
+values = (12, [31, 43, 65], [23, 42, 33, 54])
+values[1].append(95)
+print(values)
+print(values[2][1:3])
+print(values[2][:3])
+print(values[2][:])
+print(values[2][::2])
